9aa30212ceed1a6b0be1a94f0cacdf88a0c2a5ad
George Antoniadis <george@noodles.gr>
2015-03-02 17:58:22 +0200

    Update theme and templates

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,11 +1,3 @@
-/*!
- * Bootstrap v3.1.0 (http://getbootstrap.com)
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- */
-
-if (typeof jQuery === 'undefined') { throw new Error('Bootstrap requires jQuery') }
-
 /* ========================================================================
  * Bootstrap: transition.js v3.1.0
  * http://getbootstrap.com/javascript/#transitions
